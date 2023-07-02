79fb6a931edbfa1fec10e0d8b62af1d4b6cdfc32
Andrew Merrin <78210129+AM1010101@users.noreply.github.com>
2023-07-02 23:00:53 +0100

    nicer sign in page

--- src/scenes/login/index.jsx
+++ src/scenes/login/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useLogin } from '../../queries/user';
+import { TextField, Button, Box } from '@mui/material';
 
 // TODO: validate email and password
 export const Login = () => {
@@ -21,28 +22,74 @@ export const Login = () => {
 
   if (loginMutation.isError) {
     // Handle login error, e.g., display an error message
+    // TODO dont do this in production
     return <div>Error: {loginMutation.error.message}</div>;
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+    <Box
+      sx={{
+        display: 'flex',
+        height: '100vh',
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+          display: 'flex',
+        }}
+      >
+        <Box
+          component="form"
+          sx={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+            maxWidth: '400px',
+            margin: '0 auto',
+            padding: '2rem',
+          }}
+          onSubmit={handleSubmit}
+        >
+          <TextField
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            variant="outlined"
+          />
+          <TextField
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            variant="outlined"
+          />
+          <Button type="submit" variant="contained" color="secondary">
+            Login
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#f5f5f5',
+        }}
+      >
+        {/* Add your splash image or content here */}
+        <img
+          src="https://images.unsplash.com/photo-1621608120544-2acdbff1b791?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=736&q=80"
+          alt="https://unsplash.com/photos/s7ll0fuKk4Q"
+          style={{
+            width: '100%',
+            objectFit: 'cover',
+          }}
         />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <button type="submit">Login</button>
-    </form>
+      </Box>
+    </Box>
   );
 };
