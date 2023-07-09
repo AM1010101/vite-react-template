70cae6763799196235eeca31e3c9e7bcaec82698
Andrew Merrin <78210129+AM1010101@users.noreply.github.com>
2023-07-09 22:20:55 +0100

    outline of homepage

--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -14,21 +14,73 @@ const Home = () => {
         justifyContent="space-between"
         alignItems="center"
       >
-        <Header title="HOME / LANDING PAGE" />
+        {/* <Header title="HOME / LANDING PAGE" /> */}
       </Box>
       <Box
         display="grid"
         gridTemplateColumns="repeat(12, 1fr)"
         gridAutoRows="140px"
         gap="20px"
+        height="100%"
       >
         <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
+          gridColumn="span 6 "
+          // backgroundColor={colors.primary[400]}
           display="flex"
-          alignItems="center"
+          alignItems="start"
           justifyContent="center"
-        ></Box>
+          height="100%"
+        >
+          <div>
+            <h1>WELCOME TO LEARN WITH GPT</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quisquam voluptatum, quibusdam, voluptate, quia voluptas
+              quod quos voluptatibus quae doloribus quidem voluptatem.
+              Quisquam voluptatum, quibusdam, voluptate, quia voluptas
+              quod quos voluptatibus quae doloribus quidem voluptatem.
+            </p>
+            <h1>Another Great Feature</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quisquam voluptatum, quibusdam, voluptate, quia voluptas
+              quod quos voluptatibus quae doloribus quidem voluptatem.
+              Quisquam voluptatum, quibusdam, voluptate, quia voluptas
+              quod quos voluptatibus quae doloribus quidem voluptatem.
+            </p>
+            <h1>Call to Action</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quisquam voluptatum, quibusdam, voluptate, quia voluptas
+              quod quos voluptatibus quae doloribus quidem voluptatem.
+              Quisquam voluptatum, quibusdam, voluptate, quia voluptas
+              quod quos voluptatibus quae doloribus quidem voluptatem.
+            </p>
+          </div>
+        </Box>
+        <Box
+          gridColumn="span 6"
+          // backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="start"
+          justifyContent="center"
+          height="100%"
+        >
+          <Box
+            backgroundColor={colors.primary[400]}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '1rem',
+              width: '100%',
+              height: '80vh',
+              margin: '0 auto',
+              padding: '2rem',
+            }}
+          >
+            Demo should go here
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
